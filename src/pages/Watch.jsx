f7b9a74c32cfee0b503956a9046f059d3f1b47c2
Tashif <officialtashif@gmail.com>
2025-10-11 15:50:21 +0530

    asdasd

--- src/pages/Watch.jsx
+++ src/pages/Watch.jsx
@@ -26,7 +26,7 @@ export default function Watch() {
       </div>
 
       {/* Main Content */}
-      <div className="flex-1 grid lg:grid-cols-[2fr_1fr] grid-cols-1 gap-6 p-6 overflow-hidden">
+      <div className="flex-1 grid lg:grid-cols-[2fr_1fr] grid-cols-1 gap-6 p-2 lg:p-6 overflow-hidden">
         {/* Left: Video + Participants */}
         <div className="flex flex-col gap-6">
           <video
